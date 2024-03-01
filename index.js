d6f4bcbd412ad9fb96c68746e649b090ded19547
Akash Singh <akashsunilsingh5555@gmail.com>
2024-03-01 10:10:55 +0530

    fornt-end

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
+
+// Configure CORS to only accept requests from your frontend domain
+app.use(cors({
+  origin: 'https://giveway.mygiveway.tech',
+  optionsSuccessStatus: 200, // For legacy browser support
+}));
+
+app.use(express.json()); // For parsing application/json
 
 const { GITHUB_TOKEN, REPO_OWNER, REPO_NAME, FILE_PATH } = process.env;
 
@@ -15,14 +24,12 @@ app.post('/like', async (req, res) => {
     const decodedContent = Buffer.from(fileResponse.data.content, 'base64').toString();
     const currentLikes = parseInt(decodedContent, 10);
 
-    // Check if parsing resulted in NaN
     if (Number.isNaN(currentLikes)) {
       console.error('Current like count is not a number:', decodedContent);
       return res.status(400).json({ success: false, message: 'Current like count is not a number', details: decodedContent });
     }
 
     const updatedLikes = currentLikes + 1;
-
     const updatedContent = Buffer.from(updatedLikes.toString()).toString('base64');
 
     await axios.put(`https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/${FILE_PATH}`, {
@@ -35,8 +42,8 @@ app.post('/like', async (req, res) => {
 
     res.json({ success: true, updatedLikes });
   } catch (error) {
-    console.error('Failed to update like count:', error.response ? error.response.data : error);
-    res.status(500).json({ success: false, message: 'An error occurred.', details: error.response ? error.response.data : error });
+    console.error('Failed to update like count:', error);
+    res.status(500).json({ success: false, message: 'An error occurred.', details: error.message });
   }
 });
 
